0632d048e1aea6727ecbc4869fc859f02a24add6
A <anchl@users.noreply.github.com>
2019-11-07 19:39:17 -0800

    Update fff.js

--- fff.js
+++ fff.js
@@ -1,3 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    	console.log('I am here!');
+	// client.user.setPresence({ game: { name: 'Shikon no Tama', type:3 }});
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
+
 // https://stackoverflow.com/questions/54173978/hot-to-get-a-role-through-reactions
 <CLient>.on("messageReactionAdd", (reaction, user) => {
   if (user.bot) return;
