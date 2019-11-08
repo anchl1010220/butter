57590d1314a4b993bf9094b3bcd8d43ad39a0bb1
A <anchl@users.noreply.github.com>
2019-11-07 23:15:38 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,6 +45,7 @@ client.on('message', message => {
 
 client.on('messageReactionAdd', (reaction, user) => {
   if (reaction.emoji.name === "<:eggplanthand:642242790855802881>") {
-    message.member.addRole('565667223600103435').catch(console.error);
+//    message.member.addRole('565667223600103435');
+      message.channel.send('test');
   }
 });
